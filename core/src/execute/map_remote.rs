3c32b39008a606ebc5ac097eb6106956c266f3d9
Devin D'Angelo <devinjdangelo@gmail.com>
2024-03-03 08:56:12 -0500

    add remote mapping tests

--- core/src/execute/map_remote.rs
+++ core/src/execute/map_remote.rs
@@ -77,4 +77,116 @@ fn apply_info_substitutions(
 }
 
 #[cfg(test)]
-mod tests {}
+mod tests {
+    use std::collections::{HashMap, HashSet};
+    use std::sync::Arc;
+
+    use crate::execute::planning::EntityContext;
+    use crate::model::access_control::{ColumnPermission, RowPermission, SourcePermission};
+    use crate::model::data_stores::options::SourceOptions;
+    use crate::model::data_stores::DataField;
+    use crate::model::mappings::{Mapping, RemoteEntityMapping, RemoteInfoMapping, Transformation};
+    use crate::model::query::{InfoSubstitution, SubstitutionBlocks};
+    use arrow_schema::{DataType, Field, Schema};
+    use datafusion::sql::planner::SqlToRel;
+    use datafusion::sql::sqlparser::{dialect::AnsiDialect, parser::Parser};
+    use datafusion_federation_sql::query_to_sql;
+    use uuid::Uuid;
+
+    use crate::{
+        error::{MeshError, Result},
+        model::data_stores::{options::trino::TrinoSource, DataSource},
+    };
+
+    use super::{apply_info_substitutions, apply_source_substitutions};
+    fn test_source_substitution() -> Result<()> {
+        let sql = "select foo, bar from (select * from entityname);";
+        let dialect = AnsiDialect {};
+
+        let mut ast = Parser::parse_sql(&dialect, &sql)
+            .map_err(|e| MeshError::InvalidQuery(format!("sqlparser syntax error: {e}")))?;
+
+        let statement = ast.remove(0);
+
+        let schema = Arc::new(Schema::new(vec![
+            Field::new("foo", DataType::Utf8, false),
+            Field::new("bar", DataType::UInt8, false),
+        ]));
+
+        let context_provider = EntityContext::new("entityname", schema);
+        let sql_to_rel = SqlToRel::new(&context_provider);
+        let logical_plan = sql_to_rel.sql_statement_to_plan(statement)?;
+        let mut statement = query_to_sql(&logical_plan)?;
+
+        println!("Round trip statement: {statement}");
+
+        apply_source_substitutions(
+            &mut statement,
+            &RemoteEntityMapping{
+                id: Uuid::new_v4(),
+                sql: "select * from test".to_string(),
+                substitution_blocks: SubstitutionBlocks{
+                    info_substitutions: HashMap::new(),
+                    source_substitutions: HashMap::new(),
+                    num_capture_braces: 1,
+                },
+                relay_id: Uuid::new_v4(),
+                entity_id: Uuid::new_v4(),
+                remote_entity_name: "test".to_string(),
+                needs_subquery_transformation: true,
+            },
+        )?;
+
+        println!("Post sub statement {statement}");
+
+        assert!(
+            (statement.to_string() ==
+            "SELECT `entityname`.`foo`, `entityname`.`bar` FROM (SELECT col2, alias1.col1 FROM (SELECT * FROM test) WHERE col1 = '123')".to_string())
+            ||
+            (statement.to_string() ==
+            "SELECT `entityname`.`foo`, `entityname`.`bar` FROM (SELECT alias1.col1, col2 FROM (SELECT * FROM test) WHERE col1 = '123')".to_string())
+        );
+
+        Ok(())
+    }
+
+    #[test]
+    fn test_info_substitution() -> Result<()> {
+        let sql = "SELECT `entityname`.`foo`, `entityname`.`bar` FROM (SELECT alias1.col1, col2 FROM (SELECT * FROM test) WHERE col1 = '123')";
+        let dialect = AnsiDialect {};
+
+        let mut ast = Parser::parse_sql(&dialect, &sql)
+            .map_err(|e| MeshError::InvalidQuery(format!("sqlparser syntax error: {e}")))?;
+
+        let mut statement = ast.remove(0);
+
+        let remote_foo_map = RemoteInfoMapping{
+            remote_entity_mapping_id: Uuid::new_v4(),
+            information_id: Uuid::new_v4(),
+            info_mapped_name: "remote_info".to_string(),
+            literal_derived_field: false,
+            transformation: Transformation {
+                other_to_local_info: "{v}/100".to_string(),
+                local_info_to_other: "{v}*100".to_string(),
+                replace_from: "{v}".to_string(),
+            },
+        };
+
+        let info_map_lookup = HashMap::from_iter(vec![("foo", &remote_foo_map)]);
+
+        apply_info_substitutions(
+            &mut statement,
+            &info_map_lookup,
+            "entityname",
+        )?;
+
+        println!("Post sub statement {statement}");
+
+        assert_eq!(
+            statement.to_string() ,
+            "SELECT remote_info / 100, NULL FROM (SELECT alias1.col1, col2 FROM (SELECT * FROM test) WHERE col1 = '123')".to_string()
+        );
+
+        Ok(())
+    }
+}
